f485bc5dc372fe2be57ab7635936b56df9755d21
Osedave1234 <davidekalo05@gmail.com>
2022-07-01 15:07:59 -0700

    task 6 to task 9 update and task 0 and task 2 correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic in lower and upper case
+ * main - Prints a hexadecimal string
  *
  * Return: Always (Success)
  */
@@ -9,17 +9,17 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 
-	for (c = 'A'; c <= 'Z'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 }
