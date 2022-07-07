9372ff56e9725ee641f98ce08aed592476611918
Osedave1234 <davidekalo05@gmail.com>
2022-07-06 17:34:49 -0700

    1 task correction

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
-
+ * main - causes an infinite loop
+ * Return: 0
+ */
 int main(void)
 {
 	int i;
@@ -14,7 +13,6 @@ int main(void)
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
-
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
