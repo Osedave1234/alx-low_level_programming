5979143082bdba6db29c9185453ef016114a675e
Osedave1234 <davidekalo05@gmail.com>
2022-07-13 09:23:09 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *main- genrate password
+ *Return:nothing
  */
 int main(void)
 {
-	return (0);
+srand((unsigned int)time(NULL));
+char pass[13];
+int i;
+
+
+for (i = 0; i < 4; i++);
+{
+
+pass[3 * i] = '0' + (rand() % 10);
+char capLetter = 'A' + (rand() % 26);
+
+pass[(3 * i) + 1] = capLetter;
+char letter = 'a' + (rand() % 26)
+
+pass[(3 * i) + 2] = letter;
+}
+pass[3 * i] = '\0';
+printf("generated password : %s\n\n", pass);
+printf("\n\n");
+return (0);
 }
