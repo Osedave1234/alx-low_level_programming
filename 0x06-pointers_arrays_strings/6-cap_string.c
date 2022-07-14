cd105c29fa886b2028fd1d331fc9d75b457af8fc
Osedave1234 <davidekalo05@gmail.com>
2022-07-14 07:28:36 -0700

    task 6 correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,19 +11,18 @@ char *cap_string(char *s)
 {
 	int a = 0, i;
 	int cspc = 13;
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'}
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	while (s[a])
-	{
 		i = 0;
 
-		while (i < cspc)
-		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] -= 32;
-			i++;
-		}
-		a++;
+	while (i < cspc)
+	{
+		if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+			s[a] -= 32;
+		i++;
 	}
-	return (s);
+	a++;
+}
+return (s);
 }
