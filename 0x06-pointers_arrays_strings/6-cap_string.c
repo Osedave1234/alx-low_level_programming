2f725c55ecb22f1a24d7456c0f9d2f3b5513f952
Osedave1234 <davidekalo05@gmail.com>
2022-07-15 06:13:49 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to change the first letter of a word in uppercase
+ * *cap_string - this is awesome
+ * @s: pointer to char params
  *
- * Return: capitalizes letters
+ * Return: *s
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
-	int j;
-	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
-	i = 0;
-	while (str[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
-		j = 0;
-		while (c[j] != '\0')
-		{
-			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
-			j++;
-			i++;
-		}
-		return (str);
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+
+		for (j = 0; delimeters[j] != '\0';
+				if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] = s[i + 1] - 32;
 	}
+	return (s);
 }
